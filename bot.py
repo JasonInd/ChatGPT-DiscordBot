a0bf5179999a9a50da78a8a03b5deb561c83aefc
JasonInd <72218862+JasonInd@users.noreply.github.com>
2023-02-01 01:32:00 +0000

    Update bot.py
    Updated to support official ChatGPT API

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord, os, json, aiohttp
 import typing, asyncio, functools
 from discord import app_commands
-from revChatGPT.ChatGPT import Chatbot
+from revChatGPT.Official import AsyncChatbot
 
 if not os.path.exists("config.json"):
 	with open("config.json", "w+") as f:
@@ -13,22 +13,10 @@ def load_config():
 
 config = load_config()
 DISCORD_TOKEN = config['discord_bot_token']
-SESSION_TOKEN = config['session_token']
+OPEN_AI_KEY = config['open_ai_key']
 
-chatbot = Chatbot({
-  "session_token": SESSION_TOKEN
-})
+chatbot = AsyncChatbot(api_key=OPEN_AI_KEY)
 
-def to_thread(func: typing.Callable) -> typing.Coroutine:
-    @functools.wraps(func)
-    async def wrapper(*args, **kwargs):
-        return await asyncio.to_thread(func, *args, **kwargs)
-    return wrapper
-
-@to_thread
-def get_response(message, conversationid, parentid):
-    response = chatbot.ask(message, conversation_id=conversationid, parent_id=parentid)
-    return response
 
 class aclient(discord.Client):
 	def __init__(self):
@@ -48,38 +36,38 @@ class aclient(discord.Client):
 client = aclient()
 
 @client.tree.command(name="chat",description="Talk to chatGPT")
-@app_commands.describe(message = "The message you want to say to chatGPT", conversationid = "Conversation ID to continue a previous conversation (specify a parentid as well)", 
-parentid = "ID of the original message (specify a conversationid as well)", new_conversation = "Lets you start a new conversation thread")
-async def chat(interaction: discord.Interaction, message: str, conversationid: str = None, parentid: str = None, new_conversation: bool = False):
+@app_commands.describe(message = "The message you want to say to chatGPT", new_conversation = "Lets you start a new conversation thread")
+async def chat(interaction: discord.Interaction, message: str, new_conversation: bool = False):
     await interaction.response.defer()
 
     try:
         if new_conversation == True:
-            chatbot.reset_chat()
-        response = await get_response(message, conversationid, parentid)
-        if len(response['message']) > 3800:
-            split_message1 = response['message'][:3800]
-            split_message2 = response['message'][3800:].strip()
+            chatbot.reset()
+        response = await chatbot.ask(message)
+
+        if len(response['choices'][0]['text']) > 3800:
+            split_message1 = response['choices'][0]['text'][:3800]
+            split_message2 = response['choices'][0]['text'][3800:].strip()
             embed1 = discord.Embed(description = f"`{message}`\n{split_message1}")
-            embed1.set_footer(text=f"Conversation ID: {response['conversation_id']} Parent ID: {response['parent_id']}")
+            embed1.set_footer(text=f"Prompt Tokens: {response['usage']['prompt_tokens']} │ Completion Tokens: {response['usage']['completion_tokens']} │ Total Tokens Used: {response['usage']['total_tokens']}")
             embed2 = discord.Embed(description = f"`{message} continued...`\n{split_message2}")
-            embed2.set_footer(text=f"Conversation ID: {response['conversation_id']} Parent ID: {response['parent_id']}")
+            embed2.set_footer(text=f"Prompt Tokens: {response['usage']['prompt_tokens']} │ Completion Tokens: {response['usage']['completion_tokens']} │ Total Tokens Used: {response['usage']['total_tokens']}")
             await interaction.followup.send(embeds=[embed1,embed2])
 
         else:
-            embed = discord.Embed(description = f"`{message}`\n{response['message']}")
-            embed.set_footer(text=f"Conversation ID: {response['conversation_id']} Parent ID: {response['parent_id']}")
+            embed = discord.Embed(description = f"`{message}`\n{response['choices'][0]['text']}")
+            embed.set_footer(text=f"Prompt Tokens: {response['usage']['prompt_tokens']} │ Completion Tokens: {response['usage']['completion_tokens']} │ Total Tokens Used: {response['usage']['total_tokens']}")
             await interaction.followup.send(embed=embed)
     except Exception as e:
         print(e)
         await interaction.followup.send("Something went wrong, please try again! \nIf the problem persists, let me know on Github: https://github.com/JasonInd/chatGPT-DiscordBot/issues")
 
-@client.tree.command(name="refresh", description="Refresh the chatGPT session token")
-async def refresh(interaction: discord.Interaction):
+@client.tree.command(name="rollback", description="Go back a specified number of messages")
+async def refresh(interaction: discord.Interaction, amount:int):
 	try:
-		chatbot.refresh_session()
-		print("Session refreshed.")
-		await interaction.response.send_message("Session token refreshed",ephemeral=True)
+		chatbot.rollback(amount)
+		print(f"Rolled back chat history by {amount} messages")
+		await interaction.response.send_message(f"Rolled back chat history by {amount} messages",ephemeral=True)
 	except Exception as e:
 		await interaction.response.send_message("An error has occured please try again",ephemeral=True)
 		
